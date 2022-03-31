fb1761e2d744107ed5ca00f3d2fb6bd7b0382a91
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-31 13:33:12 +0200

    complete snack 2

---
+++ snack_2/main.js
@@ -0,0 +1,15 @@
+/*
+
+Snack2
+Inserisci un numero, se è pari stampa il numero,
+se è dispari stampa il numero successivo.
+
+*/
+
+
+let number = 5;
+
+while (number % 2 === 0) {
+    console.log(number)
+number++    
+}

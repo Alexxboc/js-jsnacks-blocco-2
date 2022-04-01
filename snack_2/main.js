4d887c7533aa1f6944ddbf22df8faa549f0cbf63
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-01 09:53:08 +0200

    add comments

--- snack_2/main.js
+++ snack_2/main.js
@@ -7,8 +7,11 @@ se è dispari stampa il numero successivo.
 */
 
 // Inizializzo una variabile con un numero
-let number = 2;
+let number = 3;
 
+// Creo una condizione che stampi in console il numero se è pari o stampi il numero con incremento di 1 se il numero è dispari
 if(number % 2 === 0){
     console.log(number)
+} else {
+    console.log(++number)
 }

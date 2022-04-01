c4a4735726d98d7c063155686f209e48989cb3bc
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-01 09:47:28 +0200

    add comments

--- snack_2/main.js
+++ snack_2/main.js
@@ -7,10 +7,8 @@ se è dispari stampa il numero successivo.
 */
 
 // Inizializzo una variabile con un numero
-let number = 5;
+let number = 2;
 
-// Creo nun ciclo while che stamperà in console solo quando il numero è pari
-while (number % 2 === 0) {
+if(number % 2 === 0){
     console.log(number)
-number++    
 }
